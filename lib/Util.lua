5ce15c8a089742f90dbc0a95e2512063327afa5d
howmanysmall <lhamil29@bruinmail.slcc.edu>
2019-01-22 12:41:11 -0700

    Update Util.lua
    Should be quite a bit faster, but at the cost of a lot readability.

--- lib/Util.lua
+++ lib/Util.lua
@@ -17,50 +17,46 @@ end
 -- Shorthands
 local vec3 = Vector3.new
 local cframe = CFrame.new
-local zeroVector = vec3(0, 0, 0)
 local uz = vec3(0, 0, 1)
 local ux = vec3(1, 0, 0)
-local sqrt = math.sqrt
-local dot = zeroVector.Dot
-local cross = zeroVector.Cross
 
 --[[
 -- Unused because too expensive to be worth the benefit since you'd have to call it often
 local function RealignArcToCamera(self)
-    local relativeCamPos = self.cframe:inverse() * workspace.CurrentCamera.CFrame.p
-    for _, v in pairs(self.segments) do
-        local c = v.CFrame
-        local po = c.p
-        local up = c.upVector
-        local lf = cross(relativeCamPos - po, up).unit
-        local fr = cross(lf, up).unit
-        v.CFrame = cframe(po.x, po.y, po.z, lf.x, up.x, fr.x, lf.y, up.y, fr.y, lf.z, up.z, fr.z)
-    end
+	local relativeCamPos = self.cframe:inverse() * workspace.CurrentCamera.CFrame.p
+	for _, v in pairs(self.segments) do
+		local c = v.CFrame
+		local po = c.p
+		local up = c.upVector
+		local lf = cross(relativeCamPos - po, up).unit
+		local fr = cross(lf, up).unit
+		v.CFrame = cframe(po.x, po.y, po.z, lf.x, up.x, fr.x, lf.y, up.y, fr.y, lf.z, up.z, fr.z)
+	end
 end
 ]]
 
 -- Making a CFrame value from an origin position and unit direction:
 function Util.makeOrientation(sourcePos, dir)
-    -- Construct CFrame to rotate parts from x-axis alignment (where they are constructed) to
-    -- the world orientation of the arc from a quaternion rotation that describes the
-    -- rotation of ux-->axis
-    dir = dir.unit
+	-- Construct CFrame to rotate parts from x-axis alignment (where they are constructed) to
+	-- the world orientation of the arc from a quaternion rotation that describes the
+	-- rotation of ux-->axis
+	dir = vec3(dir.X / ((dir.X * dir.X + dir.Y * dir.Y + dir.Z * dir.Z) ^ 0.5), dir.Y / ((dir.X * dir.X + dir.Y * dir.Y + dir.Z * dir.Z) ^ 0.5), dir.Z / ((dir.X * dir.X + dir.Y * dir.Y + dir.Z * dir.Z) ^ 0.5))
 
-    -- Proportionate angle between ux and direction:
-    local angleUxAxis = dot(ux, dir)
+	-- Proportionate angle between ux and direction:
+	local angleUxAxis = ux.X * dir.X + ux.Y * dir.Y + ux.Z * dir.Z --dot(ux, dir)
 
-    if (angleUxAxis > 0.99999) then
-        return cframe(sourcePos, sourcePos - uz)
-    elseif (angleUxAxis < -0.99999) then
-        return cframe(sourcePos, sourcePos + uz)
-    else
-        local q = cross(ux, dir)
-        local qw = 1 + angleUxAxis
-        local qnorm = sqrt(q.magnitude ^ 2 + qw * qw)
-        q = q / qnorm
-        qw = qw / qnorm
-        return cframe(sourcePos.x, sourcePos.y, sourcePos.z, q.x, q.y, q.z, qw)
-    end
+	if (angleUxAxis > 0.99999) then
+		return cframe(sourcePos, sourcePos - uz)
+	elseif (angleUxAxis < -0.99999) then
+		return cframe(sourcePos, sourcePos + uz)
+	else
+		local q = vec3(ux.Y * dir.Z - ux.Z * dir.Y, ux.Z * dir.X - ux.X * dir.Z, ux.X * dir.Y - ux.Y * dir.X) --cross(ux, dir)
+		local qw = 1 + angleUxAxis
+		local qnorm = ((q.Magnitude * q.Magnitude) + qw * qw) ^ 0.5
+		q = q / qnorm
+		qw = qw / qnorm
+		return cframe(sourcePos.X, sourcePos.Y, sourcePos.Z, q.X, q.Y, q.Z, qw)
+	end
 end
 
 return Util
